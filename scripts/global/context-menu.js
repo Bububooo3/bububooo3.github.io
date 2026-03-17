c9cbb4b09b596b0a2d53fcc1da8e337bfc36f944
Retroid <bububooo3@gmail.com>
2026-03-17 00:32:57 -0400

    update again again

---
+++ scripts/global/context-menu.js
@@ -0,0 +1,60 @@
+// Made w/ tutorial: https://dev.to/shantanu_jana/custom-right-click-context-menu-in-javascript-4112
+let events = ["contextmenu", "touchstart"];
+var timeout;
+var lastTap = 0;
+let contextMenu = document.getElementById("context-menu");
+
+events.forEach((eventType) => {
+  document.addEventListener(
+    eventType,
+    (e) => {
+      e.preventDefault();
+      let mouseX = e.clientX || e.touches[0].clientX;
+      let mouseY = e.clientY || e.touches[0].clientY;
+      let menuHeight = contextMenu.getBoundingClientRect().height;
+      let menuWidth = contextMenu.getBoundingClientRect().width;
+      let width = window.innerWidth;
+      let height = window.innerHeight;
+      if (width - mouseX <= 200) {
+        contextMenu.style.borderRadius = "5px 0 5px 5px";
+        contextMenu.style.left = width - menuWidth + "px";
+        contextMenu.style.top = mouseY + "px";
+        if (height - mouseY <= 200) {
+          contextMenu.style.top = mouseY - menuHeight + "px";
+          contextMenu.style.borderRadius = "5px 5px 0 5px";
+        }
+      } else {
+        contextMenu.style.borderRadius = "0 5px 5px 5px";
+        contextMenu.style.left = mouseX + "px";
+        contextMenu.style.top = mouseY + "px";
+        if (height - mouseY <= 200) {
+          contextMenu.style.top = mouseY - menuHeight + "px";
+          contextMenu.style.borderRadius = "5px 5px 5px 0";
+        }
+      }
+      contextMenu.style.visibility = "visible";
+    },
+    { passive: false },
+  );
+});
+
+document.addEventListener("touchend", function (e) {
+  var currentTime = new Date().getTime();
+  var tapLength = currentTime - lastTap;
+  clearTimeout(timeout);
+  if (tapLength < 500 && tapLength > 0) {
+    contextMenu.style.visibility = "hidden";
+    e.preventDefault();
+  } else {
+    timeout = setTimeout(function () {
+      clearTimeout(timeout);
+    }, 500);
+  }
+  lastTap = currentTime;
+});
+
+document.addEventListener("click", function (e) {
+  if (!contextMenu.contains(e.target)) {
+    contextMenu.style.visibility = "hidden";
+  }
+});
